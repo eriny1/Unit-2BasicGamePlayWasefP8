91ecc28d516f30c71377a5f7ab887a10e1b8fc65
eriny1 <erenynader734@gmail.com>
2025-03-26 15:28:25 -0700

    unit2
    Make animals into prefabs

--- unit2Wasef/Assets/Scripts/DestroyOutOfBounds.cs
+++ unit2Wasef/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveForward : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
-    public float speed = 40.0f;
+    private float topBound = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,9 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+        if (transform.position.z > topBound) 
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- unit2Wasef/Assets/Scripts/PlayerController.cs
+++ unit2Wasef/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontzlinput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +17,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < xRange)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontzlinput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontzlinput * Time.deltaTime * speed);
+       transform.Translate(Vector3.right * horizontzlinput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
+
 }
