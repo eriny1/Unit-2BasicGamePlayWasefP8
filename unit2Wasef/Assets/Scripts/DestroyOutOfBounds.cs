ff9701b017c1e974a46b0ab8080af1b5eb9ea33a
eriny1 <erenynader734@gmail.com>
2025-03-28 15:26:43 -0700

    unit2
    Collision Decisions

--- unit2Wasef/Assets/Scripts/DestroyOutOfBounds.cs
+++ unit2Wasef/Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +16,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound) 
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
+        else if (transform.position.z < lowerBound) 
+        { Destroy(gameObject);}
+        }
     }
-}
+
